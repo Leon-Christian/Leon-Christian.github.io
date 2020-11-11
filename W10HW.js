ea37c916a96cf6d6fe913ed6765526bbbe6b5e1b
Leon_Christian <lcc469@nyu.edu>
2020-11-11 14:54:59 -0500

    Update

--- W10HW.js
+++ W10HW.js
@@ -10,6 +10,7 @@ function next() {
         theTxt.textContent = "For example a ~medium~ such as a phone host multiple different forms of ~media~ such as songs, photos, videos, etc. McLuhan says that"
     } else if (x === 3) {
         theTxt.textContent = "'...All media work us over completely. They are so pervasive in their personal, political, economic, aesthetic, psychological, moral, ethical, and social consequences that they leave no part of us un- touched, unaffected, unaltered. The medium is the massage. Any understanding of social and cultural change is impossible without a knowledge of the way media work as environments.' Thus media does not exist outside of the effects of the medium that it is presented in but rather the effect the media has on us in intrinsically tied to the medium through which it is presented"
+        nextButton.textContent = "Done"
     } 
 }
 
